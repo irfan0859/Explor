c5e845dc23051ca7b5e6ae67f9ec02599e8c83be
irfan0859 <30881484+irfan0859@users.noreply.github.com>
2018-09-25 10:47:23 +0300

    Update SPlus.lua

--- plugins/SPlus.lua
+++ plugins/SPlus.lua
@@ -1,5 +1,5 @@
 --Begin Tools.lua :)
-local SUDO = 559152990 -- put Your ID here! <===
+local SUDO = 465511496 -- put Your ID here! <===
 function exi_files(cpath)
     local files = {}
     local pth = cpath
